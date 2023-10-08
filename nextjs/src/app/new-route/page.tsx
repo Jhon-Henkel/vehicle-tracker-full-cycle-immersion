77ec0c7fd79f6865bbfb1d85c17a55bcc2ad7cf5
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-08 12:32:45 -0300

    Permitindo traçar rotas no mapa

--- nextjs/src/app/new-route/page.tsx
+++ nextjs/src/app/new-route/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import type { FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js"
+import type { DirectionsResponseData, FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js"
 import { FormEvent, useRef } from "react"
 import { useMap } from "../hooks/useMap"
 
@@ -32,7 +32,19 @@ export function NewRoutePage() {
         const directionsResponse = await fetch(
             `http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`
         )
-        const directionsData = await directionsResponse.json()
+        const directionsData: DirectionsResponseData & {request: any} = await directionsResponse.json()
+        await map?.addRouteWithIcons({
+            routeId: '1',
+            startMarkerOptions: {
+                position: directionsData.routes[0].legs[0].start_location,
+            },
+            endMarkerOptions: {
+                position: directionsData.routes[0].legs[0].end_location,
+            },
+            carMarkerOptions: {
+                position: directionsData.routes[0].legs[0].start_location,
+            }
+        })
     }
     return (
         <div style={{display: "flex", flexDirection: "row", height: "100%", width: "100%"}}>
