705be46b58d8ad08dc4d9fbb99b3520b3eb11da8
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-10 19:16:30 -0300

    Fazendo com que salve as rotas no websocket

--- nestjs/src/routes/routes/routes.gateway.ts
+++ nestjs/src/routes/routes/routes.gateway.ts
@@ -1,11 +1,16 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
 import { Socket } from 'socket.io';
+import { RoutesDriverService } from '../routes-driver/routes-driver.service';
 
 @WebSocketGateway({cors: {origin: '*'}})
 export class RoutesGateway {
+
+  constructor(private routesDriverService: RoutesDriverService) {}
+
   @SubscribeMessage('new-points')
-  handleMessage(client: Socket, payload: {route_id: string, lat: number, lng: number}) {
-    client.broadcast.emit('admin-new-points', payload);
-    client.broadcast.emit(`new-points/${payload.route_id}`, payload);
+  async handleMessage(client: Socket, payload: {route_id: string, lat: number, lng: number}) {
+    await this.routesDriverService.createOrUpdate(payload)
+    client.broadcast.emit('admin-new-points', payload)
+    client.broadcast.emit(`new-points/${payload.route_id}`, payload)
   }
 }
